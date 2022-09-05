90c54e2354311d283459ed1c0453ef5b4010d350
zgold33 <zgold33@gmail.com>
2022-09-05 15:00:28 +0300

    Initial Commit

---
+++ DeliveryFood2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryFood2022
+//
+//  Created by Сергей Золотухин on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
